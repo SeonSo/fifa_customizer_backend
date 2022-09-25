e1c6b60af38dd29e0e60b143796bf336154a290c
SeonSo <tjsgh4500@gmail.com>
2022-09-26 08:29:43 +0900

    refactor jwt

--- src/main/java/com/exam/fifa/security/config/jwt/JwtProperties.java
+++
@@ -1,8 +0,0 @@
-package com.exam.fifa.security.config.jwt;
-
-public interface JwtProperties {
-    String SECRET = "SeonSo";
-    Integer EXPIRATION_TIME = 1000*60*60*24*1000;
-    String TOKEN_PREFIX = "Bearer ";
-    String HEADER_STRING = "Authorization";
-}
